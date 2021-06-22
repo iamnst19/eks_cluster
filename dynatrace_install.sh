5c50e8d67e9e7991e6d950663207d3f41a3e6a1d
Nithin Saji Thomas <50364434+iamnst19@users.noreply.github.com>
2021-06-22 10:09:26 +0530

    Update dynatrace_install.sh

--- dynatrace_install.sh
+++ dynatrace_install.sh
@@ -2,8 +2,8 @@
 WGET="/usr/bin/wget"
 WGET1="wget https://github.com/dynatrace/dynatrace-operator/releases/latest/download/install.sh"
 API_URL="https://xsj12315.live.dynatrachtte.com/api"
-API_TOKEN=ZHQwYzAxLlRLUjdaQkI3UkpERFlIUU01WVdFVUVCTy5GUlZCV1REUlVaQVdGUFRLN1o3VFRBN0tWUDc3Qk5WT1g3NExXSkZLUzM0U0tMVVZCNUlHNlpCSU9DNTNYQkNO
-PAAS_TOKEN=ZHQwYzAxLlhUM1VPSVo2SVVVN0xDNkNMMktSQUJBUy4yWFBNM0FGUU5MNEg2MzZMNTdKUDZIU0JKRldJTlNXMlZRR09PVEZPMldLUFlOQlpaSko0TkpOSEk1NVhNM1M2
+API_TOKEN=
+PAAS_TOKEN=
 CLUSTER_NAME="eks_cluster"
 INSTALL_FILE= "install.sh"
 #Downloading official Dynatrace operator script 
@@ -27,7 +27,7 @@ if [[ -f "$INSTALL_FILE" ]]; then
     
     echo "Download of script is successfull".
     echo "Initiating script execution"
-    sh ./install.sh --api-url $API_URL --api-token "dt0c01.DD5FGSRYRIPG657SLKLYTJIS.MKVUJZCRRCQQRKJDFZF5OG6ZQOWNRIA2GG4RWUS465Z7LW7F7RXN2JH5HLCGPWHO" --paas-token "dt0c01.J4TZF642SJGAQJJDHKMNCSBW.ANSPRUCFXGDOYJM7R3F6LCEYBPBWDIZOXIMFZ7WCMVWR3EUYVECVRSHBO4KIALLZ" --cluster-name "demo"
+    sh ./install.sh --api-url $API_URL --api-token "" --paas-token "" --cluster-name "demo"
     ##sh ./${INSTALL_FILE}
     ##    -api-url  ${API_URL} 
     ##    --api-token $(eval echo ${API_TOKEN} | base64 --decode)
